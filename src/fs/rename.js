f228c8f71f3ccf218310a54473ad629774ac46d8
Radionov Vitaly <radionovv89@gmail.com>
2023-06-16 22:02:55 +0300

    feat: add solution for fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,17 @@
-const rename = async () => {
-    // Write your code here 
+import { rename } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const rn = async () => {
+    const pathToFile = dirname(fileURLToPath(import.meta.url)) + '/files/wrongFilename.txt';
+    const pathToFilesCopy = dirname(fileURLToPath(import.meta.url)) + '/files/properFilename.md';
+    const msgErr = 'FS operation failed';
+
+    try {
+        await rename(pathToFile, pathToFilesCopy);
+    } catch (err) {
+        throw new Error(msgErr);
+    }
 };
 
-await rename();
+await rn();
