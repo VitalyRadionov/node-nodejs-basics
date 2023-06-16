94add0bdefd86b3733d070324a9817ad409d947c
Radionov Vitaly <radionovv89@gmail.com>
2023-06-16 19:45:22 +0300

    feat: add solution for fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+    const pathToFile = dirname(fileURLToPath(import.meta.url)) + '/files/fresh.txt';
+    const text = 'I am fresh and young';
+    const msgErr = 'FS operation failed';
+
+    try {
+        await writeFile(pathToFile, text, { flag: 'wx' });
+    } catch (err) {
+        throw new Error(msgErr);
+    }
 };
 
 await create();
