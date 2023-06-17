4d5fb68855afa53aa63fac089e507a009b8b6601
Radionov Vitaly <radionovv89@gmail.com>
2023-06-17 16:04:07 +0300

    feat: add solution for cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+    for (const key in env) {
+        if (key.startsWith('RSS')) {
+            console.log(`${key}=${env[key]}`);
+        }
+    }
 };
 
 parseEnv();
