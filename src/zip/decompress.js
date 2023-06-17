ed0af26f0bdf90d6d989b020f5572dff3003ec59
Radionov Vitaly <radionovv89@gmail.com>
2023-06-17 23:15:48 +0300

    feat: add solution for zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { pipeline } from 'node:stream';
+import { createUnzip } from 'node:zlib';
+
 const decompress = async () => {
-    // Write your code here 
+    const destinationPath = new URL('./files/fileToCompress.txt', import.meta.url);
+    const sourcePath = new URL('./files/fileToCompress.txt.gz', import.meta.url);
+    const unzip = createUnzip();
+    const source = createReadStream(sourcePath);
+    const destination = createWriteStream(destinationPath);
+
+    pipeline(source, unzip, destination, (err) => {
+        if (err) {
+            console.error('An error occurred:', err);
+            process.exitCode = 1;
+        }
+    });
 };
 
 await decompress();
