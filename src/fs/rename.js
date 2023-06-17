f3084d0c6a513bd575689b18af40d3611ebdd812
Radionov Vitaly <radionovv89@gmail.com>
2023-06-17 16:41:42 +0300

    fix: rename env in fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,17 +1,17 @@
-import { rename } from 'node:fs/promises';
+import { rename as setNewName } from 'node:fs/promises';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 
-const rn = async () => {
+const rename = async () => {
     const pathToFile = dirname(fileURLToPath(import.meta.url)) + '/files/wrongFilename.txt';
     const pathToFilesCopy = dirname(fileURLToPath(import.meta.url)) + '/files/properFilename.md';
     const msgErr = 'FS operation failed';
 
     try {
-        await rename(pathToFile, pathToFilesCopy);
+        await setNewName(pathToFile, pathToFilesCopy);
     } catch (err) {
         throw new Error(msgErr);
     }
 };
 
-await rn();
+await rename();
