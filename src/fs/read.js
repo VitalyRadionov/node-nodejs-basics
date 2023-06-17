34b363c6df564a4a74ec0fdb38b51e1d695b272e
Radionov Vitaly <radionovv89@gmail.com>
2023-06-17 15:21:15 +0300

    feat: add solution for fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = dirname(fileURLToPath(import.meta.url)) + '/files/fileToRead.txt';
+    const msgErr = 'FS operation failed';
+
+    try {
+        const files = await readFile(pathToFile, { encoding: 'utf-8' });
+        console.log(files);
+    } catch (err) {
+        throw new Error(msgErr);
+    }
 };
 
 await read();
