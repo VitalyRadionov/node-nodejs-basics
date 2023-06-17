f48ebad8370954fdd13acb188f22f3d7bdf4e824
Radionov Vitaly <radionovv89@gmail.com>
2023-06-17 17:15:38 +0300

    fix: change output format cli:env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,11 +1,13 @@
 import { env } from 'node:process';
 
 const parseEnv = () => {
+    let res = [];
     for (const key in env) {
         if (key.startsWith('RSS')) {
-            console.log(`${key}=${env[key]}`);
+            res.push(`${key}=${env[key]}`);
         }
     }
+    console.log(res.join('; '));
 };
 
 parseEnv();
