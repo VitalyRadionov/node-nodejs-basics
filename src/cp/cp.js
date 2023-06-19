4e9bceeb2bc4fbf3f9f10043d4dd76ca64b521e3
Radionov Vitaly <radionovv89@gmail.com>
2023-06-19 13:53:39 +0300

    feat: add stdin, stdout

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -2,8 +2,11 @@ import { fork } from 'node:child_process';
 
 const spawnChildProcess = async (args) => {
     const src = new URL('./files/script.js', import.meta.url);
-    fork(src, args.split(' '));
+    const cp = fork(src, args, { silent: true });
+
+    process.stdin.pipe(cp.stdin);
+    cp.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess('--version --help');
+spawnChildProcess(['--version', '--help']);
