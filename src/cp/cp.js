171ddd8a43c7a9d4ee9c2ce0fcac6970516affd3
Radionov Vitaly <radionovv89@gmail.com>
2023-06-19 13:41:49 +0300

    feat: add solution for cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
+import { fork } from 'node:child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const src = new URL('./files/script.js', import.meta.url);
+    fork(src, args.split(' '));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('--version --help');
